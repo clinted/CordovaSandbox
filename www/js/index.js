2cd150e8404926e0e38e52f00df186697a003b8a
Clint Edmonson <clint.edmonson@live.com>
2013-10-11 15:03:33 -0500

    Cleaned up unwanted files and set up project structure and setting.

--- www/js/index.js
+++ www/js/index.js
@@ -17,10 +17,12 @@
  * under the License.
  */
 var app = {
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -28,6 +30,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
@@ -35,6 +38,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
